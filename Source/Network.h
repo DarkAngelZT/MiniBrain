2d9f4ab410535da338343e0846db9cf48f9f0339
Neo <lzt777@163.com>
2025-02-14 02:54:56 +0800

    network logic

--- Source/Network.h
+++ Source/Network.h
@@ -9,10 +9,21 @@ namespace MiniBrain
 {
     class Network
     {
-    private:
-        /* data */
+    protected:
+        std::vector<Layer *> m_layers;
+        LossFunc *m_lossFunc;
     public:
         Network(/* args */) {}
         ~Network() {}
+
+        void AddLayer(Layer *layer)
+        {
+            m_layers.push_back(layer);
+        }
+
+        void SetLossFunc(LossFunc *lossFunc)
+        {
+            m_lossFunc = lossFunc;
+        }
     };
 } // namespace MiniBrain
