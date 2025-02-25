0fad8ff02b641e8e511296ca9bd279cdedc6b5dc
MBA\haisenbao <lzt777@163.com>
2025-02-25 21:12:19 +0800

    gru bug修正

--- Source/Layers/GRU.h
+++ Source/Layers/GRU.h
@@ -7,6 +7,7 @@ namespace MiniBrain
     class GRU : public Layer
     {
     protected:
+        int m_hiddenSize;
         Matrix m_z;
         Matrix m_r;
         Matrix m_h;
@@ -18,20 +19,26 @@ namespace MiniBrain
         Matrix m_weight_r;
         Matrix m_weight_h;
 
+        Vector m_bias_z;
+        Vector m_bias_r;
+        Vector m_bias_h;
+
         Matrix m_Uz;
         Matrix m_Ur;
         Matrix m_Uh;
         
         Matrix m_dWz,m_dWr,m_dWh;
         Matrix m_dUz,m_dUr,m_dUh;
+        Vector m_dbz,m_dbr,m_dbh;
         Matrix m_din;
 
         Matrix Sigmoid(const Matrix& InData)
         {
             return 1.0f/(1.0f + (-InData.array()).exp());
         }
+
     public:
-        GRU (int inSize,int OutSize):Layer(inSize,OutSize)
+        GRU (int inSize, int hiddenSize):Layer(inSize,hiddenSize),m_hiddenSize(hiddenSize)
         {
 
         }
@@ -40,34 +47,39 @@ namespace MiniBrain
         virtual void Forward(const Matrix& InData) override
         {
             const int nobs = InData.cols();
-            //out = w .* in + b
             m_h_prev = m_h;
-            m_z.noalias() = Sigmoid(m_weight_z.transpose() * InData + m_Uz.transpose() * m_h_prev);
-            m_r.noalias() = Sigmoid(m_weight_r.transpose() * InData + m_Ur.transpose() * m_h_prev);
-            m_h_tilde.array() = (m_weight_h.transpose() * InData + m_Ur.transpose() * (m_r.array() * m_h_prev.array()).matrix()).array().tanh();
+            m_z.noalias() = Sigmoid((m_weight_z.transpose() * InData + m_Uz.transpose() * m_h_prev).colwise()+m_bias_z);
+            m_r.noalias() = Sigmoid((m_weight_r.transpose() * InData + m_Ur.transpose() * m_h_prev).colwise()+m_bias_r);
+            m_h_tilde.array() = ((m_weight_h.transpose() * InData + m_Uh.transpose() * (m_r.array() * m_h_prev.array()).matrix()).colwise() + m_bias_h).array().tanh();
             m_h.array() = (1.0f - m_z.array())*m_h_prev.array() + m_z.array()*m_h_tilde.array();
         }
 
         virtual void Backward(const Matrix& InData, const Matrix& BackpropData) override
         {
             const int nobs = InData.cols();
-            Matrix m_dz(m_inSize,nobs),m_dr(m_inSize,nobs),m_dh(m_inSize,nobs);
+            Matrix m_dz(m_hiddenSize,nobs),m_dr(m_hiddenSize,nobs),m_dh(m_hiddenSize,nobs);
             m_dz.array() = BackpropData.array() * (m_h_tilde.array() - m_h.array()) * m_z.array() * (1.0f-m_z.array());
             m_dh.array() = BackpropData.array() * m_z.array() * (1.0f-m_h_tilde.array().square());
-            m_dr.array() = (m_weight_h*m_dh).array()*m_h.array()*m_r.array()*(1.0f-m_r.array());
+            m_dr.array() = (m_Uh*m_dh).array()*m_h.array()*m_r.array()*(1.0f-m_r.array());
             
             //dot(x.T, dh)
             m_dWh.noalias() = m_dh*InData.transpose();
             //dot(r*h).T*dh
             m_dUh.noalias() = m_dh*(m_r.array()*m_h.array()).matrix().transpose();
+            m_dbh.noalias() = m_dh.rowwise().mean();
+
             m_dWr.noalias() = m_dr*InData.transpose();
             //dot(m_h.T, m_dr) 
             m_dUr.noalias() = m_dr*m_h.transpose();
+            m_dbr.noalias() = m_dr.rowwise().mean();
+
             m_dWz.noalias() = m_dz*InData.transpose();
             //dot(m_h.T, m_dz)
-            m_dUz.noalias() = m_dz * m_h.transpose();
+            m_dUz.noalias() = m_dz*m_h.transpose();
+            m_dbz.noalias() = m_dz.rowwise().mean();
+
             m_din.resize(m_inSize,nobs);
-            m_din.noalias() = m_Uh.transpose() * m_dh + m_Ur.transpose() * m_dr + m_Uz.transpose() * m_dz;
+            m_din.noalias() = m_weight_h * m_dh + m_weight_r * m_dr + m_weight_z * m_dz;
         }
 
         virtual const Matrix& Output() const override
@@ -82,24 +94,29 @@ namespace MiniBrain
 
         virtual void Init() override
         {
-            m_weight_z.resize(m_inSize,m_outSize);
-            m_weight_r.resize(m_inSize,m_outSize);
-            m_weight_h.resize(m_inSize,m_outSize);
-            m_Uz.resize(m_inSize,m_outSize);
-            m_Ur.resize(m_inSize,m_outSize);
-            m_Uh.resize(m_inSize,m_outSize);
+            m_weight_z.resize(m_inSize,m_hiddenSize);
+            m_weight_r.resize(m_inSize,m_hiddenSize);
+            m_weight_h.resize(m_inSize,m_hiddenSize);
+            m_Uz.resize(m_hiddenSize,m_hiddenSize);
+            m_Ur.resize(m_hiddenSize,m_hiddenSize);
+            m_Uh.resize(m_hiddenSize,m_hiddenSize);
+            m_bias_z.resize(m_hiddenSize);
+            m_bias_r.resize(m_hiddenSize);
+            m_bias_h.resize(m_hiddenSize);
 
-            m_h_prev.resize(m_outSize,1);
-            m_h_tilde.resize(m_outSize,1);
-            m_h.resize(m_outSize,1);
+            m_h_prev.resize(m_hiddenSize,1);
+            m_h_tilde.resize(m_hiddenSize,1);
+            m_h.resize(m_hiddenSize,1);
 
-            m_dWz.resize(m_inSize,m_outSize);
-            m_dWr.resize(m_inSize,m_outSize);
-            m_dWh.resize(m_inSize,m_outSize);
-            m_dUz.resize(m_inSize,m_outSize);
-            m_dUr.resize(m_inSize,m_outSize);
-            m_dUz.resize(m_inSize,m_outSize);
-            m_din.resize(m_inSize,m_outSize);
+            m_dWz.resize(m_inSize,m_hiddenSize);
+            m_dWr.resize(m_inSize,m_hiddenSize);
+            m_dWh.resize(m_inSize,m_hiddenSize);
+            m_dUz.resize(m_hiddenSize,m_hiddenSize);
+            m_dUr.resize(m_hiddenSize,m_hiddenSize);
+            m_dUh.resize(m_hiddenSize,m_hiddenSize);
+            m_dbz.resize(m_hiddenSize);
+            m_dbr.resize(m_hiddenSize);
+            m_dbh.resize(m_hiddenSize);
         }
 
         virtual void Init(const float& mu, const float& sigma, Random& RNG) override
@@ -121,23 +138,29 @@ namespace MiniBrain
             AlignedMapVec Uz(m_Uz.data(), m_Uz.size());
             AlignedMapVec Ur(m_Ur.data(), m_Ur.size());
             AlignedMapVec Uh(m_Uh.data(), m_Uh.size());
+            AlignedMapVec Bz(m_bias_z.data(), m_bias_z.size());
+            AlignedMapVec Br(m_bias_r.data(), m_bias_r.size());
+            AlignedMapVec Bh(m_bias_z.data(), m_bias_z.size());
             opt.Update(ConstAlignedMapVec(m_dWz.data(), m_dWz.size()), Wz);
             opt.Update(ConstAlignedMapVec(m_dWr.data(), m_dWr.size()), Wr);
             opt.Update(ConstAlignedMapVec(m_dWh.data(), m_dWh.size()), Wh);
             opt.Update(ConstAlignedMapVec(m_dUz.data(), m_dUz.size()), Uz);
             opt.Update(ConstAlignedMapVec(m_dUr.data(), m_dUr.size()), Ur);
             opt.Update(ConstAlignedMapVec(m_dUh.data(), m_dUh.size()), Uh);
+            opt.Update(ConstAlignedMapVec(m_dbz.data(), m_dbz.size()), Bz);
+            opt.Update(ConstAlignedMapVec(m_dbr.data(), m_dbr.size()), Br);
+            opt.Update(ConstAlignedMapVec(m_dbh.data(), m_dbh.size()), Bh);
         }
 
         void SetBatchSize(int Size)
         {
             //隐状态数据比较特殊，不能随意改变batch大小，否则会导致数据丢失
-            m_h.resize(m_outSize, Size);
-            m_h_prev.resize(m_outSize, Size);
-            m_h_tilde.resize(m_outSize, Size);
+            m_h.resize(m_hiddenSize, Size);
+            m_h_prev.resize(m_hiddenSize, Size);
+            m_h_tilde.resize(m_hiddenSize, Size);
 
-            m_z.resize(m_outSize,Size);
-            m_r.resize(m_outSize,Size);
+            m_z.resize(m_hiddenSize,Size);
+            m_r.resize(m_hiddenSize,Size);
         }
 
         void ResetMemory()
